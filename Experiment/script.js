062c58a6584b9b1971456381ee06aa236ce1746a
Nicolas Sampedro <nxs310@miami.edu>
2021-04-18 22:56:17 -0400

    Side bar nav
    Trying to figure out how to make the side bar nav match

--- Experiment/script.js
+++ Experiment/script.js
@@ -1,3 +1,8 @@
-function myFunction(x) {
-  x.classList.toggle('change');
+function openNav() {
+  document.getElementById('myNav').style.width = '100%';
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
 }
